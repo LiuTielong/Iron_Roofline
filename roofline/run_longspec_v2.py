7eb20c58f8c8623e71047c9083ed6fd7117a915a
LiuTielong <liutl18@outlook.com>
2025-04-27 10:02:45 +0800

    把longspec的树式投机采样结果总结了

--- roofline/run_longspec_v2.py
+++ roofline/run_longspec_v2.py
@@ -57,8 +57,36 @@ def main():
                             accepted_lengths=AATs, save_path=f"Figures/longspec_all/chain/{prefill_len}_combined_model.png", batch_size=1, ori_x=5)
         i += 1
 
-
     # step 2: 针对树形投机采样，数据来自表3-4.xlsx
+    df = pd.read_excel(data_dir2, skiprows=2, header=None)
+    gammas = df.iloc[:, 0].tolist()
+    verify_lens = [gamma + 1 for gamma in gammas]   # 大模型校验时的输入数据长度
+    AATs_all = df.iloc[:, 1:].values.T.tolist()           # AAT数据，存为2D列表
+    i = 0
+    for prefill_len in prefill_lens:
+        AATs = AATs_all[i]
+        # 计算draft, verify的时间
+        verify_times = []
+        draft_times = []
+        args.prompt_len = prefill_len
+        for total_token, AAT in zip(verify_lens, AATs):
+            aat = int(AAT)
+            # 小模型的draft阶段
+            _, _, _, fused_draft_cycles = longspec_draft_cycles_comp(args, input_len=aat, kv_len=args.prompt_len)
+            # 大模型的verify阶段
+            _, _, _, fused_verify_cycles = llama3_cycles_comp(args, input_len=total_token, kv_len=args.prompt_len)
+            draft_time = fused_draft_cycles / (args.clock_frequency * 1e6)
+            verify_time = fused_verify_cycles / (args.clock_frequency * 1e6)
+            verify_times.append(verify_time)
+            draft_times.append(draft_time)
+        draw_roofline(prefill_lengths=verify_lens, times=verify_times, save_path=f"Figures/longspec_all/tree/{prefill_len}_roofline_model.png")
+        draw_roofline_discount(prefill_lengths=verify_lens, verify_times=verify_times, draft_times=draft_times, 
+                            save_path=f"Figures/longspec_all/tree/{prefill_len}_discounted_roofline_model.png")
+        draw_acc(prefill_lengths=verify_lens, accepted_lengths=AATs, save_path=f"Figures/longspec_all/tree/{prefill_len}_acc.png")
+        draw_combined_model(prefill_lengths=verify_lens, verify_times=verify_times, draft_times=draft_times, 
+                            accepted_lengths=AATs, save_path=f"Figures/longspec_all/tree/{prefill_len}_combined_model.png", batch_size=1, ori_x=69)
+        # 因为原始配置是[4,16,16,16,16], 节点数为1+68=69.
+        i += 1
 
 if __name__ == "__main__":
     main()
